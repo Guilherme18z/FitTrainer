e6b68a2a2e48df96b742e65281b8586d5314ba85
Guilherme18z <237462023@eniac.edu.br>
2024-06-04 22:57:58 -0300

    Perfil Commit (atualizado)

--- app/src/main/java/com/example/portfolio/Treinos.java
+++ app/src/main/java/com/example/portfolio/Treinos.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +25,54 @@ public class Treinos extends AppCompatActivity {
             return insets;
         });
 
+        TextView supinoReto, supinoInclinado, peckDeck, Crossover, supinoDeclinado;
+
+        supinoReto = findViewById(R.id.supinoReto);
+        supinoInclinado = findViewById(R.id.supinoInclinado);
+        peckDeck = findViewById(R.id.PeckDeck);
+        Crossover = findViewById(R.id.Crossover);
+        supinoDeclinado = findViewById(R.id.supinoDeclinado);
+
+
+        supinoReto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Treinos.this, treino_supino_reto.class);
+                startActivity(intent);
+            }
+        });
+
+        supinoInclinado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Treinos.this, treino_supino_inclinado.class);
+                startActivity(intent);
+            }
+        });
+
+        peckDeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Treinos.this, treino_peckDeck.class);
+                startActivity(intent);
+            }
+        });
+
+        Crossover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Treinos.this, treino_crossover.class);
+                startActivity(intent);
+            }
+        });
+
+        supinoDeclinado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Treinos.this, treino_supino_declinado.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
