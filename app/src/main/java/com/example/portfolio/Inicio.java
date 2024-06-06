4c85934b8b3db026628b008574a21d1eb2605944
BrunoLima88 <lima.bruno88@gmail.com>
2024-06-06 19:10:35 -0300

    atualização

--- app/src/main/java/com/example/portfolio/Inicio.java
+++ app/src/main/java/com/example/portfolio/Inicio.java
@@ -3,22 +3,60 @@ package com.example.portfolio;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Inicio extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.framelayout);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int itemId = menuItem.getItemId();
+
+                if (itemId == R.id.home) {
+                    loadFragment(new HomeFragment(), false);
+
+                } else if (itemId == R.id.treinos) {
+                    loadFragment(new TreinosFragment(), false);
+
+                } else if (itemId == R.id.perfil) {
+                    loadFragment(new PerfilFragment(), false);
+                } else {
+                    loadFragment(new ConfigFragment(), false);
+
+                }
+
+                loadFragment(new HomeFragment(), true);
+                return true;
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,4 +70,21 @@ public class Inicio extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
     }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.framelayout, fragment);
+
+        } else {
+            fragmentTransaction.replace(R.id.framelayout, fragment);
+        }
+
+
+
+        fragmentTransaction.commit();
+    }
+
 }
