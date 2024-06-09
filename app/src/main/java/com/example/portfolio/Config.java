50ddcada10f895c5af4c4940645682b30776c8c2
Mayconhd7 <Mayconhd7@gmail.com>
2024-06-09 20:22:31 -0300

    Atualização

--- app/src/main/java/com/example/portfolio/Config.java
+++ app/src/main/java/com/example/portfolio/Config.java
@@ -3,7 +3,7 @@ package com.example.portfolio;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +18,8 @@ public class Config extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_config);
+
+        // Ajustar o padding para considerar as barras do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/portfolio/ConfigFragment.java
+++ app/src/main/java/com/example/portfolio/ConfigFragment.java
@@ -1,5 +1,6 @@
 package com.example.portfolio;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class ConfigFragment extends Fragment {
@@ -17,6 +19,21 @@ public class ConfigFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_config, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_config, container, false);
+
+        TextView btnSair;
+
+
+        btnSair = view.findViewById(R.id.btnSair);
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirecionar para a nova atividade (substitua "MinhaAtividade.class" pelo nome da sua atividade)
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
